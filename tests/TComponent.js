46799ab9553150a631dd33418ca1eec080dc4522
haiix <haiix268@gmail.com>
2020-09-18 14:39:32 +0900

    Add tests

--- tests/TComponent.js
+++ tests/TComponent.js
@@ -71,3 +71,84 @@ describe('TComponent.parse()', () => {
     expect(() => { TComponent.parse('<p a=1>Attribute value does not start with "</p>'); }).throw(Error);
   });
 });
+
+
+describe('TComponent.build()', () => {
+  it('Element', () => {
+    const nodes = TComponent.parse(`
+      <label id="nameOfPet">
+        <span>Please enter the name of your pet.</span>
+        <input onchange="handleChange(event)" />
+      </label>
+    `);
+    const element = TComponent.build(nodes[0]);
+    expect(element).to.be.a('HTMLLabelElement');
+    expect(element.getAttribute('id')).to.equal('nameOfPet');
+    expect(element.childElementCount).to.equal(2);
+    expect(element.children[0]).to.be.a('HTMLSpanElement');
+    expect(element.children[0].textContent).to.equal('Please enter the name of your pet.');
+    expect(element.children[1]).to.be.a('HTMLInputElement');
+    expect(element.children[1].getAttribute('onchange')).to.be.a('string');
+    expect(element.children[1].onchange).to.be.a('function');
+  });
+  it('Component', () => {
+    const nodes = TComponent.parse(`
+      <label id="nameOfPet">
+        <span>Please enter the name of your pet.</span>
+        <input onchange="this.handleChange(event)" />
+      </label>
+    `);
+    const thisObj = {
+      modified: false,
+      handleChange(event) {
+        this.modified = true;
+      }
+    };
+    const element = TComponent.build(nodes[0], thisObj);
+    expect(element).to.be.a('HTMLLabelElement');
+    expect(element.getAttribute('id')).to.equal(null);
+    expect(thisObj.nameOfPet).to.equal(element);
+    expect(element.childElementCount).to.equal(2);
+    expect(element.children[0]).to.be.a('HTMLSpanElement');
+    expect(element.children[0].textContent).to.equal('Please enter the name of your pet.');
+    expect(element.children[1]).to.be.a('HTMLInputElement');
+    expect(element.children[1].getAttribute('onchange')).to.equal(null);
+    expect(element.children[1].onchange).to.be.a('function');
+
+    const handleChange = element.children[1].onchange;
+    expect(thisObj.modified).to.equal(false);
+    handleChange();
+    expect(thisObj.modified).to.equal(true);
+  });
+});
+
+
+describe('Extends TComponent', () => {
+  it('Extends', () => {
+    class SubComponent extends TComponent {
+      template() {
+        return `
+          <label id="nameOfPet">
+            <span>Please enter the name of your pet.</span>
+            <input onchange="this.handleChange(event)" />
+          </label>
+        `;
+      }
+      constructor(attr, nodes) {
+        super();
+        this.modified = false;
+      }
+      handleChange(event) {
+        this.modified = true;
+      }
+    }
+
+    expect(SubComponent.parsedTemplate).to.equal(undefined);
+    const subComponent = new SubComponent();
+    expect(SubComponent.parsedTemplate).to.be.an('object');
+    expect(subComponent.element).to.be.a('HTMLLabelElement');
+
+  });
+});
+
+
