e77be9ff6777cb737e1d6c208e720b77df156e74
haiix <haiix268@gmail.com>
2026-08-05 10:52:08 +0900

    fix: update entry resolution in vite config to use import.meta.dirname

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
       fileName: 'index',
       formats: ['es'],
     },
